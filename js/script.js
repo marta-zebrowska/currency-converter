f7271365b82e341b17cdee5cea2bdc737a0bd0e5
Marta Zebrowska-Giers <zeberita@gmail.com>
2022-10-28 10:10:18 +0200

    changes in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("Cześć!");
-
 const currencyElement = document.querySelector(".js-currency");
 const rateElement = document.querySelector(".js-rate");
 const formElement = document.querySelector(".js-form");
@@ -7,19 +5,24 @@ const productElement = document.querySelector(".js-product");
 const resultElement = document.querySelector(".js-result");
 const submitElement = document.querySelector(".js-submit");
 
-formElement.addEventListener("submit", (event) => {
-event.preventDefault();
+const calculateProduct = (event) => {
+  event.preventDefault();
 
-const currency = currencyElement.value;
-const rate = rateElement.value;
+  const currency = currencyElement.value;
+  const rate = rateElement.value;
+  
+  const product = currency / rate;
+  productElement.innerText = product.toFixed(2);
+};
 
-let product = currency / rate;
+const changeText = () => {
+ resultElement.innerText = "Gratulacje! Posiadasz ";
+};
 
-productElement.innerText = product.toFixed(2);
-});
-
-submitElement.addEventListener("click", () => {
-  resultElement.classList.remove("hide");
-
-  resultElement.innerText = "Gratulacje! Posiadasz ";
-    });
+    const init = () => {
+      console.log("Cześć!");
+      formElement.addEventListener("submit", calculateProduct, changeText); 
+      formElement.addEventListener("submit", changeText); 
+    };  
+    
+    init();
